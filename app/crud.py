701b9378c2d10657d993cf4bd3e50a88d63f2f2c
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-13 17:42:33 +0200

    Implemet 'get_mission_by_name' function.

--- app/crud.py
+++ app/crud.py
@@ -12,3 +12,7 @@ def create_mission(db: Session, mission: schemas.MissionCreate):
 
 def get_missions(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.Mission).offset(skip).limit(limit).all()
+
+
+def get_mission_by_name(db: Session, name: str):
+    return db.query(models.Mission).filter(models.Mission.name == name).first()
