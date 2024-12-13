e4499a246066bfc33fa1231b8572bb5837265a8f
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-13 17:05:06 +0200

    Fix 'Integer' spelling on 'id' Column.

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from .database import Base
 class Mission(Base):
     __tablename__ = "missions"
 
-    id = Column(Integr, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     status = Column(String)
     descritption = Column(String)
