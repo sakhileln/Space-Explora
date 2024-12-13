07a71ea170524d07b8baaa0498acbeaac5e2f58f
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-13 18:13:34 +0200

    Format the file according to PEP8.

--- app/api/spacex.py
+++ app/api/spacex.py
@@ -2,6 +2,7 @@ import requests
 
 SPACE_API_URL = "https://api.spacexdata.com/v4/launches"
 
+
 def get_spacex_launches():
     response = requests.get(SPACE_API_URL)
     if response.status_code == 200:
