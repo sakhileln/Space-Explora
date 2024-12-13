3218cfbd275590123da8b839b8e7f70a3c67578c
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-13 17:09:01 +0200

    Define the Pydantic models for request and response validation.

---
+++ app/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class MissionBase(BaseModel):
+    name: str
+    status: str
+    descritption: str
+
+
+class MissionCreate(MissionBase): ...
+
+
+class Mission(MissionBase):
+    id: int
+
+    class Config:
+        orm_mode = True
