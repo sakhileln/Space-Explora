f5a4dcb62b3cdb7a60f667035fd3478fa8015370
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-13 17:29:20 +0200

    Entry point for FastAPI app, initializes the app, routes, and endpoints.

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import models, crud, schemas, database
+from .api import nasa, spacex
+
+app = FastAPI()
+
+
+# Dependency to get database session
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.on_event("startup")
+def startup():
+    # Create tables in database
+    models.Base.metadata.create_all(bind=database.engine)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Space Nomad!"}
