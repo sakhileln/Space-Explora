eeb643f9b419f667f116520ea1921f6fe3bceed6
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-14 19:51:10 +0200

    Fix typo on SessionLocal from 'bird' to 'bind'.

--- app/database.py
+++ app/database.py
@@ -21,5 +21,5 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bird=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
