1a7e77fe354e163d4836e54b2da78c005e89d1c6
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-12-15 13:35:43 +0200

    Use the 'make_api_request' module.

--- app/api/spacex.py
+++ app/api/spacex.py
@@ -6,31 +6,8 @@ the public SpaceX data API. The API returns a list of launches, including
 details like launch dates, mission names, and more.
 """
 
-import requests
+from .make_api_request import make_api_request
 
-SPACE_API_URL = "https://api.spacexdata.com/v4/launches"
+SPACEX_API_URL = "https://api.spacexdata.com/v4/launches"
 
-
-def get_spacex_launches():
-    """
-    Fetch a list of SpaceX launches from the SpaceX public API.
-
-    This function sends a GET request to the SpaceX Launch API endpoint
-    and returns a list of launch data in JSON format. Each launch object
-    contains details about the launch, such as the mission name, date, and
-    other relevant information.
-
-    Returns:
-        list: A list of dictionaries where each dictionary represents a SpaceX launch.
-              If the request fails or the status code is not 200, None is returned.
-    """
-    try:
-        response = requests.get(SPACE_API_URL, timeout=10)
-        if response.status_code == 200:
-            return response.json()
-    except requests.exceptions.Timeout:
-        print("The request timed out.")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occured: {e}")
-
-    return None
+spacex_data = make_api_request(SPACEX_API_URL)
