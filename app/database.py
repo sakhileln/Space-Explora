7cdfd191382465e101f1ed03be123b180f16afbc
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2024-12-21 05:03:37 +0200

    Update name of database.

--- app/database.py
+++ app/database.py
@@ -18,7 +18,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "sqlite:///./missions.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
